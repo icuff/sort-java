0a34d00efebd912e6fafa26b77124cde349f0eca
bc-nogueira <bcnogueira@id.uff.br>
2019-06-16 17:11:36 -0300

    Add timer.

--- app/src/main/java/com/example/sortjava/MainActivity.java
+++ app/src/main/java/com/example/sortjava/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.sortjava;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.Random;
@@ -11,27 +12,37 @@ public class MainActivity extends AppCompatActivity {
     private final Integer tamanho = 10000;
     private final Integer numeroMax = 1000000;
     private Integer array[];
+    Button prepararBtn;
+    Button ordenarBtn;
+    TextView statusTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        this.preparaArray();
-        this.mostrarArray();
+        prepararBtn = findViewById(R.id.prepararBtn);
+        ordenarBtn = findViewById(R.id.ordenarBtn);
+        statusTxt = findViewById(R.id.statusTxt);
     }
 
-    private void preparaArray() {
+    public void preparaArray(View v) {
+        long begin = System.nanoTime();
         array = new Integer[tamanho];
         Random random = new Random();
 
         for(int i = 0; i < tamanho; i++) {
             array[i] = random.nextInt(this.numeroMax + 1) + 1;
         }
+
+        long difference = (System.nanoTime() - begin)/1000000;
+        prepararBtn.setVisibility(View.INVISIBLE);
+        ordenarBtn.setVisibility(View.VISIBLE);
+        statusTxt.setText("Finished preparing array in " + difference + "ms");
+        this.mostrarArray();
     }
 
     private void mostrarArray() {
-        TextView arrayTxt = (TextView) findViewById(R.id.arrayTxt);
+        TextView arrayTxt = findViewById(R.id.arrayTxt);
         arrayTxt.setText(this.imprimeArray());
     }
 
@@ -45,6 +56,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void ordenar(View v) {
+        long begin = System.nanoTime();
         for (int i = 0; i < this.tamanho; i++) { //Number of passes
             for (int j = 0; j < (this.tamanho - i - 1); j++) { //Notice that j < (length - i)
                 //Compare the adjacent positions
@@ -56,6 +68,10 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         }
+
+        long difference = (System.nanoTime() - begin)/1000000;
+        statusTxt.setText("Finished sorting array in " + difference + "ms");
+        ordenarBtn.setVisibility(View.INVISIBLE);
         this.mostrarArray();
     }
 }
